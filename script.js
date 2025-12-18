150377f4615dbc32d5dcdbaa9894999b1a194e64
ebosebastien68-maker <ebosebastien68@gmail.com>
2025-12-18 14:37:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 // Le client Supabase est initialisé dans supabaseClient.js
 // On récupère simplement la référence depuis window.supabaseClient
 
-let supabaseClientInstance;
-
 // Attendre que supabaseClient.js soit chargé
 if (window.supabaseClient && window.supabaseClient.supabase) {
     supabaseClientInstance = window.supabaseClient.supabase;
